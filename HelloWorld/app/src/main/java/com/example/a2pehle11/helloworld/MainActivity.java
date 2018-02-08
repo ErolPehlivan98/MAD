ef2994ea34e2056a7a9f4933736388ab71d0ae7b
2pehle11 <godofwar1234>
2018-02-08 10:17:33 +0000

    fixed git caching of HelloWorld

---
+++ HelloWorld/app/src/main/java/com/example/a2pehle11/helloworld/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.a2pehle11.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //TextView tv = new TextView(this);
+        //tv.setText("Hello World");
+        //setContentView(tv);
+    }
+}
